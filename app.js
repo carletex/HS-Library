c9041ad322c7747d32f6c8e140848d2ea8019202
carletex <oceanrdn@gmail.com>
2014-09-16 16:16:30 -0400

    Basis CRUD for Books

--- app.js
+++ app.js
@@ -26,8 +26,15 @@ app.get('/books', function(req, res) {
 
 	var books = db.collection('books');
 	books.find().toArray(function(err, result) {
-		console.log('Result', result);
 		res.render('books', {books: result});
+	});
+
+});
+
+app.get('/books/:id', function(req, res) {
+
+	db.collection('books').findById(req.params.id, function(err, result) {
+		res.render('book', {book: result, bookJSON: JSON.stringify(result)});
 	});
 
 });
@@ -35,13 +42,38 @@ app.get('/books', function(req, res) {
 app.post('/books', function(req, res) {
 
 	var books = db.collection('books');
+	var id = req.body._id;
 	var title = req.body.title;
 
-	books.insert({title: title}, function() {
-		var books = db.collection('books');
-		books.find().toArray(function(err, result) {
-			res.render('books', {books: result, addedBook: req.body.title});
+	if (id){
+		// Update
+		books.updateById(id, {title: title}, function(err, result) {
+			if (err) {
+				res.status(500).end();
+				return;
+			}
+			console.log('update->', result);
+			var books = db.collection('books');
+			books.find().toArray(function(err, result) {
+				res.render('books', {books: result, addedBook: req.body.title});
+			});
 		});
+	} else{
+		// Create
+		books.insert({title: title}, function() {
+			var books = db.collection('books');
+			books.find().toArray(function(err, result) {
+				res.render('books', {books: result, addedBook: req.body.title});
+			});
+		});
+	}
+
+});
+
+app.get('/books/delete/:id', function(req, res) {
+
+	db.collection('books').removeById(req.params.id, function(err, result) {
+		res.end('Deleted!');
 	});
 
 });
@@ -50,6 +82,20 @@ app.get('/books/add', function(req, res) {
 	res.render('add-book');
 });
 
+app.get('/books/edit/:id', function(req, res) {
+
+	db.collection('books').findById(req.params.id, function(err, result) {
+		res.render('add-book', {book: result});
+	});
+
+});
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 app.listen(8000, function() {
     console.log('Listening on port 8000...');
