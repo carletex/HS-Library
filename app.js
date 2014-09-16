e5a726d4d9fb2b538e9844445327aa5550a7637b
carletex <oceanrdn@gmail.com>
2014-09-16 17:16:40 -0400

    Refactor: Change routes to a folder

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var mongo = require('mongoskin');
 var db = mongo.db('mongodb://localhost/library', {native_parser:true});
 
+var routes = require('./routes/index');
+var books = require('./routes/books');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -16,87 +19,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-
-// Routes
-app.get('/', function(req, res) {
-	res.render('index');
+// Make our db accessible to our router
+app.use(function(req,res,next) {
+	req.db = db;
+	next();
 });
 
-app.get('/books', function(req, res) {
-
-	var books = db.collection('books');
-	books.find().toArray(function(err, result) {
-		res.render('books', {books: result});
-	});
-
-});
-
-app.get('/books/:id', function(req, res) {
-
-	db.collection('books').findById(req.params.id, function(err, result) {
-		res.render('book', {book: result, bookJSON: JSON.stringify(result)});
-	});
-
-});
-
-app.post('/books', function(req, res) {
-
-	var books = db.collection('books');
-	var id = req.body._id;
-	var title = req.body.title;
-
-	if (id){
-		// Update
-		books.updateById(id, {title: title}, function(err, result) {
-			if (err) {
-				res.status(500).end();
-				return;
-			}
-			console.log('update->', result);
-			var books = db.collection('books');
-			books.find().toArray(function(err, result) {
-				res.render('books', {books: result, addedBook: req.body.title});
-			});
-		});
-	} else{
-		// Create
-		books.insert({title: title}, function() {
-			var books = db.collection('books');
-			books.find().toArray(function(err, result) {
-				res.render('books', {books: result, addedBook: req.body.title});
-			});
-		});
-	}
-
-});
-
-app.get('/books/delete/:id', function(req, res) {
-
-	db.collection('books').removeById(req.params.id, function(err, result) {
-		res.end('Deleted!');
-	});
-
-});
-
-app.get('/books/add', function(req, res) {
-	res.render('add-book');
-});
-
-app.get('/books/edit/:id', function(req, res) {
-
-	db.collection('books').findById(req.params.id, function(err, result) {
-		res.render('add-book', {book: result});
-	});
-
-});
+app.use('/', routes);
+app.use('/books', books);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 app.listen(8000, function() {
-    console.log('Listening on port 8000...');
+	console.log('Listening on port 8000...');
 });
---
+++ routes/books.js
@@ -0,0 +1,107 @@
+/* jshint node: true */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+
+	var books = req.db.collection('books');
+	books.find().toArray(function(err, result) {
+		if (err) {
+			res.status(500).end();
+			return;
+		}
+
+		res.render('books', {books: result});
+	});
+
+});
+
+router.get('/add', function(req, res) {
+	console.log('here');
+	res.render('add-book');
+});
+
+router.post('/', function(req, res) {
+
+	var books = req.db.collection('books');
+	var id = req.body._id;
+	var title = req.body.title;
+
+	if (id){
+
+		// Update
+		books.updateById(id, {title: title}, function(err) {
+			if (err) {
+				res.status(500).end();
+				return;
+			}
+
+			var books = req.db.collection('books');
+			books.find().toArray(function(err, result) {
+				res.render('books', {books: result, addedBook: req.body.title});
+			});
+
+		});
+
+	} else {
+
+		// Create
+		books.insert({title: title}, function(err) {
+			if (err) {
+				res.status(500).end();
+				return;
+			}
+
+			var books = req.db.collection('books');
+			books.find().toArray(function(err, result) {
+				res.render('books', {books: result, addedBook: req.body.title});
+			});
+
+		});
+
+	}
+
+});
+
+router.get('/:id', function(req, res) {
+
+	req.db.collection('books').findById(req.params.id, function(err, result) {
+		if (err) {
+			res.status(500).end();
+			return;
+		}
+
+		res.render('book', {book: result, bookJSON: JSON.stringify(result)});
+	});
+
+});
+
+router.get('/edit/:id', function(req, res) {
+
+	req.db.collection('books').findById(req.params.id, function(err, result) {
+		if (err) {
+			res.status(500).end();
+			return;
+		}
+
+		res.render('add-book', {book: result});
+	});
+
+});
+
+router.get('/delete/:id', function(req, res) {
+
+	req.db.collection('books').removeById(req.params.id, function(err) {
+		if (err) {
+			res.status(500).end();
+			return;
+		}
+
+		res.redirect('/books');
+	});
+
+});
+
+module.exports = router;
